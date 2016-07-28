46bebe77eac8243612c26e70543044d339f8c3d6
Michelangelo De Simone <michelangelo@mozilla.com>
2016-07-28 14:47:45 -0700

    Adding gcc to the toolchain under tools/avr/gcc.

--- tools/build/build-toolchain.sh
+++ tools/build/build-toolchain.sh
@@ -4,6 +4,7 @@ BASEDIR=$(cd $(dirname $0)/../.. && pwd)
 OUTDIR=$BASEDIR/out
 OBJDIR=$OUTDIR/obj
 BASEAVRDIR=$BASEDIR/tools/avr
+NUMCORES=$(getconf _NPROCESSORS_ONLN)
 
 mkdir -p $OBJDIR > /dev/null 2>&1
 
@@ -12,8 +13,16 @@ echo "Building binutils..."
 sleep 1
 mkdir $OBJDIR/binutils > /dev/null 2>&1
 cd $OBJDIR/binutils
-$BASEAVRDIR/binutils-gdb/configure --target=avr --prefix=$OUTDIR/usr/local/avr --disable-nls --enable-install-libbfd && make && make install
+$BASEAVRDIR/binutils-gdb/configure --target=avr --prefix=$OUTDIR/usr/local/avr --disable-nls --enable-install-libbfd && make -j$NUMCORES && make install
 echo "Done"
 
-# TODO: gcc, avr-libc, avrdude, gdb, simulavrxx?
+# gcc
+echo "Building gcc..."
+sleep 1
+mkdir $OBJDIR/gcc > /dev/null 2>&1
+cd $OBJDIR/gcc
+$BASEAVRDIR/gcc/configure --target=avr --prefix=$OUTDIR/usr/local/avr --disable-nls --enable-languages=c,c++ --disable-libssp && make -j$NUMCORES && make install
+echo "Done"
+
+# TODO: avr-libc, avrdude, gdb, simulavrxx?
 # See: http://www.mbeckler.org/microcontrollers/avrgcc_build/
